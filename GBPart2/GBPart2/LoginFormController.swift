810bf3e6f75545a41216b249640d0b9108ef4708
AntonovaOlgaGB <73314414+AntonovaOlgaGB@users.noreply.github.com>
2020-12-16 04:16:02 +0300

    Work in progress

---
+++ GBPart2/GBPart2/LoginFormController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginFormController.swift
+//  GBPart2
+//
+//  Created by Olya on 16.12.2020.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginButtomPressed(_ sender: Any) {
+    }
+    
+}
