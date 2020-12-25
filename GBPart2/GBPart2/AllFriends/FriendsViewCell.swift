e78c7aecf6e303ac283a84e110dfd4830e92d361
AntonovaOlgaGB <73314414+AntonovaOlgaGB@users.noreply.github.com>
2020-12-25 03:03:01 +0300

    Workinprogress3

---
+++ GBPart2/GBPart2/AllFriends/FriendsViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FriendsViewCell.swift
+//  GBPart2
+//
+//  Created by Olya on 25.12.2020.
+//
+
+import UIKit
+
+class FriendsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
