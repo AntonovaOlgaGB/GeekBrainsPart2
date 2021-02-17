fd12196f0101976a171ffb8ed2a4647c4fca904d
AntonovaOlgaGB <73314414+AntonovaOlgaGB@users.noreply.github.com>
2021-02-18 00:43:11 +0300

    Обновление от 18.02

---
+++ GBPart2/GBPart2/AllFriends/AllFriendsView.swift
@@ -0,0 +1,20 @@
+//
+//  AllFriendsView.swift
+//  GBPart2
+//
+//  Created by Olya on 18.02.2021.
+//
+
+import UIKit
+
+class AllFriendsView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
